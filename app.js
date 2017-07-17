13c96e92351aae71acbca6dfdcc9271e71dc6ac1
Kyle Richmond <kylerichmond79@gmail.com>
2017-07-17 11:41:47 -0700

    day 2 mvp

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 'use strict';
+$(document).ready(function() {
+  // hamburger handler
+  $(function(){
+    $('.fa-navicon').on('click', function(event){
+      event.preventDefault();
+      $('.navList').slideToggle();
+    })
+  })
+});
+
 function projectEntry(rawDataObj) {
   this.title = rawDataObj.title;
   this.completedOn = rawDataObj.completedOn;
